a5bb9b0ca064d25d047bb1d90aa71c956969cca4
ohalmanacs <ohalmanacs@fastmail.com>
2020-05-09 23:36:58 -0400

    Add more expectations to counter test

--- src/counter.spec.js
+++ src/counter.spec.js
@@ -2,15 +2,20 @@ import Counter from './Counter.svelte'
 import { render, fireEvent } from '@testing-library/svelte'
 
 it('it works', async () => {
-  const { getByText, getByTestId } = render(Counter);
+  const { getByText } = render(Counter);
 
   const increment = getByText('Inc');
   const decrement = getByText('Dec');
   const counter = getByText(/^Clicked/);
 
+  expect(counter.textContent).toBe('Clicked 0');
+
   await fireEvent.click(increment);
   await fireEvent.click(increment);
   await fireEvent.click(increment);
+
+  expect(counter.textContent).toBe('Clicked 3');
+
   await fireEvent.click(decrement);
 
   expect(counter.textContent).toBe('Clicked 2');
