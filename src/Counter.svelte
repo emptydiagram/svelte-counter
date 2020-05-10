a45b9b089d8367fe88887146aa4f1cc1b7a68790
ohalmanacs <ohalmanacs@fastmail.com>
2020-05-09 23:04:28 -0400

    Counter: Add decrement action

--- src/Counter.svelte
+++ src/Counter.svelte
@@ -1,13 +1,17 @@
 <script>
   let count = 0;
 
-  function handleClick() {
+  function handleIncClick() {
       count += 1;
+  }
+  function handleDecClick() {
+      count -= 1;
   }
 
 </script>
 
 <div>
   <p>Clicked {count}</p>
-  <button on:click={handleClick}>Inc</button>
+  <button on:click={handleIncClick}>Inc</button>
+  <button on:click={handleDecClick}>Dec</button>
 </div>
